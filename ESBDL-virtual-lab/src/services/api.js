8a6edbbf41c4ed7bcb12fe880a04dbecf0cc051b
VivekPatil-2006 <vivek.j.patil2006@gmail.com.com>
2026-02-07 17:31:55 +0530

    Update Android Manifest

--- ESBDL-virtual-lab/src/services/api.js
+++ ESBDL-virtual-lab/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://untrailed-lura-transmittible.ngrok-free.dev/api",
+  //baseURL: "https://untrailed-lura-transmittible.ngrok-free.dev/api",
+  baseURL: "https://esdl-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
